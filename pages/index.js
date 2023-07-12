4b62c02028ebb76c7f24e4cec7770940378e0a23
STEPHEN MESNILDREY <48994539+AceOfSpades78@users.noreply.github.com>
2023-07-12 12:53:13 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ export default function Home() {
         <p className="description">
           GB Performance : Système Double Stimulation<code>pages/index.js</code>
         </p>
-        <iframe src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQhruFCVG80JiPbbyFhR8nXhExDx2nvGa64D5SwXbsO9ysxBEl8rZCRJCUukcVaMLMFsbxDyVknhcb1/pubhtml?gid=0&amp;single=true&amp;widget=false&amp;headers=false" style="width: 1200px;height: 1200px;"></iframe>
+        <iframe src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQhruFCVG80JiPbbyFhR8nXhExDx2nvGa64D5SwXbsO9ysxBEl8rZCRJCUukcVaMLMFsbxDyVknhcb1/pubhtml?gid=0&amp;single=true&amp;widget=false&amp;headers=false"></iframe>
     </main>
 
       <Footer />
