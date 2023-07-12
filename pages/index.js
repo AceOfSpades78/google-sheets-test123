dbbf2aef74c5bcaaf6f03a57994dccda469c1fc4
STEPHEN MESNILDREY <48994539+AceOfSpades78@users.noreply.github.com>
2023-07-12 12:40:38 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,16 +6,19 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Système Double Stimulation!</title>
+        <title>Next.js Starter!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <Header title="GB Performance!" />
-        <p className="description"></p>
-     <iframe src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQhruFCVG80JiPbbyFhR8nXhExDx2nvGa64D5SwXbsO9ysxBEl8rZCRJCUukcVaMLMFsbxDyVknhcb1/pubhtml?gid=0&amp;single=true&amp;widget=true&amp;headers=false"></iframe>
-    
-    <Footer />
+        <p className="description">
+          Get started <code>pages/index.js</code>
+        </p>
+        <iframe src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQhruFCVG80JiPbbyFhR8nXhExDx2nvGa64D5SwXbsO9ysxBEl8rZCRJCUukcVaMLMFsbxDyVknhcb1/pubhtml?gid=0&amp;single=true&amp;widget=true&amp;headers=false"></iframe>
+      </main>
+
+      <Footer />
     </div>
   )
 }
