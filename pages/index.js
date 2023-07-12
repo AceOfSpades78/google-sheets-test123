cb4579ed548cdcae98d99136d50e2de4454eb975
STEPHEN MESNILDREY <48994539+AceOfSpades78@users.noreply.github.com>
2023-07-12 13:14:10 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -9,11 +9,10 @@ export default function Home() {
         <title>GB Performance!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
       <main>
-        <iframe class="iframe" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQhruFCVG80JiPbbyFhR8nXhExDx2nvGa64D5SwXbsO9ysxBEl8rZCRJCUukcVaMLMFsbxDyVknhcb1/pubhtml?gid=0&amp;single=true&amp;widget=false&amp;headers=false"></iframe>
         <Header title="GB Performance!" />
-        <p className="description">GB Performance : Système Double Stimulation<code>pages/index.js</code></p>
+        <iframe class="iframe" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQhruFCVG80JiPbbyFhR8nXhExDx2nvGa64D5SwXbsO9ysxBEl8rZCRJCUukcVaMLMFsbxDyVknhcb1/pubhtml?gid=0&amp;single=true&amp;widget=false&amp;headers=false"></iframe>
+        <p className="description">GB Performance : Système Double Stimulation<code>pages/index.js</code></p>  
       </main>
       <Footer />
     </div>
