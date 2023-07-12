9da4bb131acd7585ea28efa51d9c68ae0f0fcc0b
STEPHEN MESNILDREY <48994539+AceOfSpades78@users.noreply.github.com>
2023-07-12 12:31:07 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,8 +13,7 @@ export default function Home() {
       <main>
         <Header title="GB Performance!" />
         <p className="description"></p>
-        <iframe src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQhruFCVG80JiPbbyFhR8nXhExDx2nvGa64D5SwXbsO9ysxBEl8rZCRJCUukcVaMLMFsbxDyVknhcb1/pubhtml?widget=false&amp;range=A1:BT108&amp;headers=false"></iframe>
-
+        <iframe width="1200" width="1200" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQhruFCVG80JiPbbyFhR8nXhExDx2nvGa64D5SwXbsO9ysxBEl8rZCRJCUukcVaMLMFsbxDyVknhcb1/pubhtml?widget=false&amp;range=A1:BT108&amp;headers=false"></iframe>
       <Footer />
     </div>
   )
